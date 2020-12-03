fa68d3b10c0adf0caedac5dbcdc603e5ee6955cb
Eelco Dijkstra <eelco.j.dijkstra@gmail.com>
2020-12-03 20:56:48 +0100

    tools en links aangepast.

--- _build/jupyter_execute/tools.py
+++ _build/jupyter_execute/tools.py
@@ -1,14 +1,17 @@
-# Tools
+# Tools en links
 
 Voor het uittesten van web-API's zijn er enkele handige hulpmiddelen, naast de Jupyter Notebooks zoals we die hier gebruiken.
 
 ## NodeRed
 
-Met NodeRed kun je op een grafische manier allerlei protocollen en API's aan elkaar verbinden.
+Met [NodeRed](https://nodered.org) kun je op een grafische manier allerlei protocollen en API's aan elkaar verbinden.
+
+Via [FRED.sensetecnic.com](https://fred.sensetecnic.com) kun je een eigen online-versie van NodeRed krijgen.
+De eenvoudige versie is gratis: deze is prima te gebruiken voor oefenen en experimenteren. De gratis server blijft 24 uur actief; daarna moet je deze eventueel herstarten. Je gegevens blijven wel bewaard.
 
 ## Postman
 
-Met Postman (desktop application) kun je API's uittesten.
+Met [Postman](https://postman.com) (desktop application) kun je API's uittesten.
 Je kunt gemakkelijk de verschillende soorten parameters instellen, en de resultaten op verschillende manieren bekijken.
 Voorbeelden en scenario's kun je bewaren.
 
@@ -19,4 +22,8 @@ Met `curl` kun je via de unix/Linux command-line HTTP-requests versturen.
 
 Er is ook een bijbehorende library die je in programma's kunt gebruiken.
 
+
+## Programmable web
+
+De website [https://www.programmableweb.com](https://www.programmableweb.com) bevat een overzicht van heel veel web-toepassingen met een web-API.
 
